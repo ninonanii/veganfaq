390477dd21c10912569c7646fc2fb5b86b665164
dom <17290358+zukinii@users.noreply.github.com>
2024-07-22 22:25:07 +0200

    base styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '$lib/styles/global.css'
+
+	import { onNavigate } from '$app/navigation'
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve()
+				await navigation.complete
+			})
+		})
+	})
+</script>
+
+<svelte:head>
+	<title>Vegan FAQ</title>
+	<meta name="description" content="Vegan FAQ" />
+	<meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
+	<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
+</svelte:head>
+
+<slot />
